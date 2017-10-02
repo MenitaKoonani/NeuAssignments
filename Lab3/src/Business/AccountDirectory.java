cd5c0edcc77abd2f78867b301a04b5c929a33610
Menita Koonani <koonani.m@husky.neu.edu>
2017-10-02 15:45:15 -0400

    Lab3 Navigation through panels.

---
+++ Lab3/src/Business/AccountDirectory.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Menita Koonani
+ */
+public class AccountDirectory {
+
+    private ArrayList<Account> accountList;
+
+    public AccountDirectory() {
+        this.accountList = new ArrayList<>();
+    }
+
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
+
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
+
+    public Account addNewAccount() {
+        Account account = new Account();
+        accountList.add(account);
+        return account;
+    }
+
+    public void deleteAccount(Account account) {
+        accountList.remove(account);
+    }
+
+    public Account searchAccount(String accountNum) {
+        for (Account account : accountList) {
+            if (accountNum.equals(account.getAccountNumber())) {
+                return account;
+            }
+        }
+        return null;
+    }
+}
